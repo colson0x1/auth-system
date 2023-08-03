b080b229383d325f5c1e8a516bb5a42be32cb9c0
Colson <colsoncalifornication@gmail.com>
2023-08-03 07:13:52 +0545

    handle express routes

--- server/index.js
+++ server/index.js
@@ -4,13 +4,15 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+const router = require('./router');
 
 // App Setup
 app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*'}))
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3009;
 const server = http.createServer(app);
-server.listen(port)
+server.listen(port);
 console.log('Server listening on:', port);
