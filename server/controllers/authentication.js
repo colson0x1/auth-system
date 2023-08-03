4cefe5d3224c16db76de36247ee941e7f449ae31
Colson <colsoncalifornication@gmail.com>
2023-08-03 14:27:48 +0545

    create user records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,25 @@
-exports.signup = function (req, res, next) {
-  res.send({ success: true });
+const User = require('../models/user');
+
+exports.signup = async function (req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password,
+    });
+
+    await user.save();
+
+    res.json({ success: true });
+  } catch (err) {
+    return next(err);
+  }
 };
