43e2153025966e722341e0542ab551fd5eab18ff
Colson <colsoncalifornication@gmail.com>
2023-08-03 13:32:45 +0545

    establish mongodb connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// DB Setup
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err));
+db.once('open', () => console.log('Connected to Database...'));
 
 const app = express();
 const router = require('./router');
