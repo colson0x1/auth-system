b3ea5f00753a02080843fd23bc97e7c2d4f70c28
Colson <colsoncalifornication@gmail.com>
2023-08-03 15:07:18 +0545

    encrypt password with bcrypt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,6 +4,12 @@ exports.signup = async function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide email and password' });
+  }
+
   try {
     const existingUser = await User.findOne({ email: email });
 
