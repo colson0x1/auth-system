28cb7244698539443e26b4926a58210966d5a06a
Colson <colsoncalifornication@gmail.com>
2023-08-03 18:36:41 +0545

    compare hashed password on login

--- server/services/passport.js
+++ server/services/passport.js
@@ -26,6 +26,14 @@ const localLogin = new LocalStrategy(localOptions, function (
     }
 
     // compare passwords - is `password` equal to user.password?
+    user.comparePassword(password, function (err, isMatch) {
+      if (err) return done(err);
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
   });
 });
 
@@ -55,3 +63,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
 
 // Tell passport to use this Strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
