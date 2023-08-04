7f3d6d5eb8bb4684f1a5702588e317d6558ad780
Colson <colsoncalifornication@gmail.com>
2023-08-04 07:50:12 +0545

    refractor old bcrypt-nodejs with new bcryptjs

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
@@ -9,37 +9,33 @@ const userSchema = new Schema({
 
 // On save hook, encrypt password
 // Run this func before saving a model
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   // get access to the user model
   const user = this;
 
-  // generate a salt then run callback
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) {
-      return next(err);
-    }
+  try {
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
 
     // hash (encrypt) our password using the salt
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
-      if (err) {
-        return next(err);
-      }
+    const hash = await bcrypt.hash(user.password, salt);
 
-      // overwrite plain text password with encrypted password
-      user.password = hash;
-      next();
-    });
-  });
+    // overwrite plain text password with encrypted password
+    user.password = hash;
+    next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
-userSchema.methods.comparePassword = function (candidatePassword, callback) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) {
-      return callback(err);
-    }
-
-    callback(null, isMatch);
-  });
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    // Compare candidatePassword with the stored password hash
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const ModelClass = mongoose.model('user', userSchema);
