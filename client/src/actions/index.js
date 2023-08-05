f7821db8b4d3e37c6c5df655f4c933299b67dd25
Colson <colsoncalifornication@gmail.com>
2023-08-05 14:50:38 +0545

    implement signin action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
+// Sign up action creator
 export const signup = (formProps, callback) => async (dispatch) => {
   try {
     const response = await axios.post(
@@ -16,6 +17,23 @@ export const signup = (formProps, callback) => async (dispatch) => {
   }
 };
 
+// Sign in action creator
+export const signin = (formProps, callback) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      'http://localhost:3009/signin',
+      formProps
+    );
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem('token', response.data.token);
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid login credentials' });
+  }
+};
+
+// Sign out action creator
 export const signout = () => {
   localStorage.removeItem('token');
 
