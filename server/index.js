70b73b0c39f5dec1e0a449282ff22953c5a97e9c
Colson <colsoncalifornication@gmail.com>
2023-08-05 12:49:26 +0545

    resolve CORS error

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -16,8 +17,20 @@ db.once('open', () => console.log('Connected to Database...'));
 const app = express();
 const router = require('./router');
 
+// CORS configuration
+const allowedOrigins = ['http://localhost:3000'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if the origin is in the allowedOrigins array
+    // or if it's undefined (for same-origin requests)
+    const isAllowed = !origin || allowedOrigins.indexOf(origin) !== -1;
+    callback(null, isAllowed);
+  },
+};
+
 // App Setup
 app.use(morgan('combined'));
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
