6109834be6d74af03078f58aaf4617bf69fa0822
Colson <colsoncalifornication@gmail.com>
2023-08-05 13:06:43 +0545

    dispatch action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { AUTH_USER } from './types';
 
-export const signup = (formProps) => (dispatch) => {
-  axios.post('http://localhost:3009/signup', formProps);
+export const signup = (formProps) => async (dispatch) => {
+  const response = await axios.post('http://localhost:3009/signup', formProps);
+
+  dispatch({ type: AUTH_USER, payload: response.data.token });
 };
 
 /*
